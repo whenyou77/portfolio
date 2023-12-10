6fdb13ed779004e1f6e06ef22b8b067410285bcd
whenyou77 <itsantongd@gmail.com>
2023-12-10 23:02:59 +0100

    Transitioned into 3D, loading maps

--- secret_santa2k23/src/secret_santa2k23.nim
+++ secret_santa2k23/src/secret_santa2k23.nim
@@ -17,7 +17,7 @@
 #
 # ****************************************************************************************
 
-import raylib, raymath, reasings, nim_tiled, std/math
+import raylib, raymath, reasings, nim_tiled, std/math, os
 
 # ----------------------------------------------------------------------------------------
 # Global Variables Definition
@@ -31,11 +31,12 @@ type
     gameOver
     thxForPlaying
   Thing = object of RootObj
-    pos: Vector2
-    size: Vector2
+    pos: Vector3
+    size: Vector3
   Actor = object of Thing
-    vel: Vector2
+    vel: Vector3
     hp: int
+    grounded: bool
   Solid = object of Thing
   Player = object of Actor
     att_cooldown: int
@@ -55,8 +56,15 @@ var transitioning = false
 
 var players: array[4,Player]
 var walls: seq[Wall] = @[]
+var level: Model
 
-var camera = Camera2D(zoom:1.0)
+var camera = Camera(
+  position: Vector3(x: 5, y: 5, z: 10),  # Camera position
+  target: Vector3(x: 0, y: 0, z: 0),     # Camera target it looks-at
+  up: Vector3(x: 0, y: 1, z: 0),         # Camera up vector (rotation over its axis)
+  fovy: 45,                              # Camera field-of-view apperture in Y (degrees)
+  projection: Orthographic           # Defines projection type, see CameraProjection
+)
 
 # function to activate transition
 proc transition(transitionTo:Screen,lengthIn:int,lengthOut:int) =
@@ -80,37 +88,49 @@ proc updateDrawFrame {.cdecl.} =
   case currentScreen:
     of gameplay:
       for i,p in players.mpairs:
-        if p.att_cooldown > 0: p.att_cooldown -= 1
+        if p.att_cooldown > 0: 
+          p.att_cooldown -= 1
+          echo p.att_cooldown
+        if p.vel.y > -2.0: p.vel.y -= 0.04
         if i == 0:
           if isKeyDown(D) or isKeyDown(Right):
-            if p.vel.x < 8.0: p.vel.x += 0.5
+            if p.vel.x < 0.5: p.vel.x += 0.075
           elif isKeyDown(A) or isKeyDown(Left):
-            if p.vel.x > -8.0: p.vel.x -= 0.5
+            if p.vel.x > -0.5: p.vel.x -= 0.075
           else:
             if p.vel.x > 0.0: 
-              p.vel.x -= 0.3
+              p.vel.x -= 0.05
               if p.vel.x < 0.0:
                 p.vel.x = 0.0
             elif p.vel.x < 0.0: 
-              p.vel.x += 0.3
+              p.vel.x += 0.05
               if p.vel.x > 0.0:
                 p.vel.x = 0.0
           if isKeyDown(S) or isKeyDown(Down):
-            if p.vel.y < 8.0: p.vel.y += 0.5
+            if p.vel.z < 0.5: p.vel.z += 0.075
           elif isKeyDown(W) or isKeyDown(Up):
-            if p.vel.y > -8.0: p.vel.y -= 0.5
+            if p.vel.z > -0.5: p.vel.z -= 0.075
           else:
-            if p.vel.y > 0.0: 
-              p.vel.y -= 0.3
-              if p.vel.y < 0.0:
-                p.vel.y = 0.0
-            elif p.vel.y < 0.0: 
-              p.vel.y += 0.3
-              if p.vel.y > 0.0:
-                p.vel.y = 0.0
-          if isMouseButtonPressed(MouseButton.Left): p.att_cooldown = 60
-        p.pos += p.vel
-      camera.offset = Vector2(x: screenWidth/2.0-players[0].pos.x, y: screenHeight/2.0-players[0].pos.y)
+            if p.vel.z > 0.0: 
+              p.vel.z -= 0.05
+              if p.vel.z < 0.0:
+                p.vel.z = 0.0
+            elif p.vel.z < 0.0: 
+              p.vel.z += 0.05
+              if p.vel.z > 0.0:
+                p.vel.z = 0.0
+          if isKeyPressed(Space) and p.grounded: p.vel.y = 1.0
+          if isMouseButtonPressed(MouseButton.Left) and p.att_cooldown == 0: p.att_cooldown = 60
+        p.grounded = false
+        p.pos.x += p.vel.x
+        p.pos.z += p.vel.z
+        p.pos.y += p.vel.y
+        if p.pos.y < 1.0: 
+          p.pos.y = 1.0
+          p.vel.y = 0.0
+          p.grounded = true
+      camera.position = players[0].pos + Vector3(x:56,y:56,z:56)
+      camera.target = players[0].pos
       if isKeyPressed(Enter): currentScreen = pause
     of pause:
       if isKeyPressed(Enter): currentScreen = gameplay
@@ -132,22 +152,24 @@ proc updateDrawFrame {.cdecl.} =
       fadeTimer=fadeOutLen
       currentScreen = nextScreen
       case currentScreen:
-        of gameplay: players[0] = Player(pos:Vector2(x:64.0,y:64.0),size:Vector2(x:32.0,y:32.0),vel:Vector2(),hp:1,att_cooldown:0)
-        else: camera.offset = Vector2(x: 0.0, y: 0.0)    
-
+        of gameplay: players[0] = Player(pos:Vector3(x: 0.0,y:1.0,z: 0.0),size:Vector3(x:2.0,y:2.0,z:2.0),vel:Vector3(),hp:1,att_cooldown:0)
+        else: camera.position = Vector3()
   # --------------------------------------------------------------------------------------
   # Draw
   # --------------------------------------------------------------------------------------
   beginDrawing()
-  beginMode2D(camera)
-  clearBackground(RayWhite)
+  clearBackground(Color(r:8,g:8,b:16,a:255))
   if currentScreen == title: drawText("Welcome! Press Enter to continue!", 20, screenHeight-60, 40, LightGray)
   # don't want to repeat the same code for rendering gameplay in pause screen
   elif currentScreen == gameplay or currentScreen == pause: 
     drawText("*Blows up pancakes with mind*\n\n\nPress Enter to open the pause screen.", 20, screenHeight-100, 40, LightGray)
+    beginMode3D(camera)
     for i,p in players.pairs:
-      drawRectangle(p.pos,p.size,Green)
-  endMode2D()
+      #drawRectangle(p.pos,p.size,Green)
+      drawCube(p.pos,p.size,Green)
+      drawModel(level, Vector3(x:0.0,y:0.0,z:0.0),4.0,White)
+      #drawGrid(1000,2.0)
+    endMode3D()
   # pause screen
   if currentScreen == pause:
     drawRectangle(0,0,screenWidth,screenHeight,Color(r:0,g:0,b:0,a:96))
@@ -164,6 +186,10 @@ proc main =
   # Initialization
   # --------------------------------------------------------------------------------------
   initWindow(screenWidth, screenHeight, "raylib [core] example - basic window")
+  level = loadModel(getAppDir() & "./dungeon_test.obj")
+  let level_tex = loadTexture(getAppDir() & "./dungeon_tileset2.png")
+  level_tex.setTextureFilter(Point)
+  level.materials[0].maps[Albedo].texture= level_tex
   when defined(emscripten):
     emscriptenSetMainLoop(updateDrawFrame, 60, 1)
   else:
